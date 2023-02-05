b21e9279c5296fa277a3bc7dfb7f95bed920a7e8
Azybekov Nuralim <nur.cost.lpoi@gmail.com>
2023-02-05 17:28:55 +0600

    ren

--- src/assets/localData/localData.js
+++ src/assets/localData/localData.js
@@ -1,10 +1,10 @@
 const radioData = ["A", "B", "C", 'D']
 const objOfForm = [
-    { name: 'name', title: 'Name', type: 'text', register: 'firstname', settings: { minLength: { value: 3, message: 'должно быть больше 3 символов' } } },
-    { name: 'surname', title: 'Surname', type: 'text', register: 'lastname', settings: { minLength: { value: 3, message: 'должно быть больше 3 символов' }, } },
-    { name: 'age', title: 'Age', type: 'number', register: 'age', settings: { min: { value: 6, message: 'минимальное значение 6' }, } },
+    { name: 'name', title: 'Name', type: 'text', register: 'firstname', settings: { minLength: { value: 3, message: 'must be more than 3 characters' } } },
+    { name: 'surname', title: 'Surname', type: 'text', register: 'lastname', settings: { minLength: { value: 3, message: 'must be more than 3 characters' }, } },
+    { name: 'age', title: 'Age', type: 'number', register: 'age', settings: { min: { value: 6, message: 'minimum value 6' }, } },
     { name: 'group', title: 'Group', type: 'radio', register: 'group', elements: radioData, settings: {} },
-    { name: 'class', title: 'Class', type: 'number', register: 'classes', settings: { min: { value: 1, message: 'минимальное значение 1' }, max: { value: 11, message: 'максимальное значение 11' }, } },
+    { name: 'class', title: 'Class', type: 'number', register: 'classes', settings: { min: { value: 1, message: 'minimum value 1' }, max: { value: 11, message: 'max value 11' }, } },
     {
         name: 'photo', title: "Photo", type: 'text', register: 'image', settings: {
             pattern: {
@@ -17,3 +17,4 @@ const objOfForm = [
 
 
 export { radioData, objOfForm }
+
